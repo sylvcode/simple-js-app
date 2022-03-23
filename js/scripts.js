62461a8d05ebb3a8c402558e5fa4033d9903e8f6
Sylvia <sylvia@donchev.is>
2022-03-23 20:32:32 +0100

    3 pokemon objects with value

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
 
-let pokemonList=[{name: "Bulbasure"},{height: "7"},{type: ["grass","poison"]}];
+let pokemonList=[{name: "Bulbasure"},{height: "7"},{type: ["grass","poison"]},{name: "Ivysaur"},{height: "1"},{type: ["monster","grass"]},{name: "Venusaur"},{height: "2"},{type: ["monster","grass"]}];
 
