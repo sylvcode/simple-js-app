eec4a02d90856c01bfcab85a592bde706238d511
Sylvia <sylvia@donchev.is>
2022-03-30 11:52:33 +0200

    forEach Loop getAll pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,97 @@
-let pokemonList = [
-  {
-    name: "Bulbasure",
-    height: 0.7,
-    types: ["grass", "poison"],
-  },
-  {
-    name: "Charizard",
-    height: 1.7,
-    types: ["monster", "dragon"],
-  },
-  {
-    name: "Ivysaur",
-    height: 1,
-    types: ["monster", "grass"],
-  },
-  {
-    name: "Venusaur",
-    height: 2,
-    types: ["monster", "grass"],
-  },
-  {
-    name: "Wartortle",
-    height: 1,
-    types: ["monster", "water 1"],
-  },
-  {
-    name: "Charmeleon",
-    height: 1.1,
-    types: ["monster", "dragon"],
-  },
-  {
-    name: "Butterfree",
-    height: 1.1,
-    types: ["bug"],
-  },
-  {
-    name: "Beedril",
-    height: 1,
-    types: ["bug"],
-  },
-  {
-    name: "Pidgeotto",
-    height: 1.1,
-    types: ["flying"],
-  },
-];
+// wrap in IIFE format
+let pokemonRepository = (function () {
+  // array list including nested objects
+  let pokemonList = [
+    {
+      name: "Bulbasure",
+      height: 0.7,
+      types: ["grass", "poison"],
+    },
+    {
+      name: "Charizard",
+      height: 1.7,
+      types: ["monster", "dragon"],
+    },
+    {
+      name: "Ivysaur",
+      height: 1,
+      types: ["monster", "grass"],
+    },
+    {
+      name: "Venusaur",
+      height: 2,
+      types: ["monster", "grass"],
+    },
+    {
+      name: "Wartortle",
+      height: 1,
+      types: ["monster", "water 1"],
+    },
+    {
+      name: "Charmeleon",
+      height: 1.1,
+      types: ["monster", "dragon"],
+    },
+    {
+      name: "Butterfree",
+      height: 1.1,
+      types: ["bug"],
+    },
+    {
+      name: "Beedril",
+      height: 1,
+      types: ["bug"],
+    },
+    {
+      name: "Pidgeotto",
+      height: 1.1,
+      types: ["flying"],
+    },
+  ];
+  // getAll Function returning PokemonList
+  function getAll() {
+    return pokemonList;
+  }
+
+  //adding add Function to the PokemonList
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+  //returning getAll and add in one {}
+  return {
+    getAll: getAll,
+    add: add,
+  };
+})();
 
 // forEach() Loops
-pokemonList.forEach(function (pokemon) {
-  document.write(
-    " Name: " +
-      pokemon.name +
-      " Height: " +
-      pokemon.height +
-      " Types: " +
-      pokemon.types
-  );
+pokemonRepository.getAll().forEach(function (pokemon) {
+  if (pokemon.height < 1.7) {
+    document.write("<p>" + pokemon.name + " " + "(" + pokemon.height + ")</p>");
+  } else if (pokemon.height >= 1.7) {
+    document.write(
+      "<p>" +
+        pokemon.name +
+        " " +
+        "(" +
+        pokemon.height +
+        ")" +
+        " Wow, tha's big!</p>"
+    );
+  }
 });
+
+// Loop that iterates over each item in pokemonList
+//for (let i = 0; i < pokemonList.length; i++) {
+//document.write(
+//  " Name: " + pokemonList[i].name + " (Height: " + pokemonList[i].height + "."
+//);
+// document.write(" Types: " + pokemonList[i].types[0]);
+
+//Conditional checks if the height is above a certain value.
+
+// if (pokemonList[i].height > 1.7) {
+//   document.write("<h4> - Wow that's big!</h4>");
+// }
+//]
+//}
