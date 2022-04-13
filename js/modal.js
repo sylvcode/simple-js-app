ade86e38f439b97adcdb47236c91ccbb5aa5de13
Sylvia Antons <67543909+sylviaantons@users.noreply.github.com>
2022-04-13 13:51:35 +0200

    modal removed and modal.js created

---
+++ js/modal.js
@@ -0,0 +1,53 @@
+// modal function 
+let modal = (function () {
+ 
+    let modalContainer = document.querySelector("#modal-container");
+    function showModal(title, text) {
+    modalContainer.innerHTML = "";
+    
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+ // Button 
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modal.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+  }
+  
+  function hideModal() {
+    mocalConatiner.classList.remove('is-visible');
+  }
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+  modalContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+  // modal ends here
+  // return statement:
+  return {
+      showModal: showModal,
+      hideModal:hideModal,
+  };
+})();
