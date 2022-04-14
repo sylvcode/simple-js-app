f654441d404ff96f1f3c69b750abd72550b6620f
Sylvia Antons <67543909+sylviaantons@users.noreply.github.com>
2022-04-14 13:01:26 +0200

    styling fixed

--- js/modal.js
+++ js/modal.js
@@ -1,20 +1,17 @@
 // modal function 
 
 
-
+//Enable specifying a title and content for the showModal function:
 let modal = (function () {
- 
-    let modalContainer = document.querySelector("#modal-container");
-    function showModal(title, text) {
-    modalContainer.innerHTML = "";
-    
+ let modalContainer = document.querySelector('#modal-container');
+  function showModal(title, text) {
+    modalContainer.innerHTML = '';
     let modal = document.createElement('div');
     modal.classList.add('modal');
 
- // Button 
     let closeButtonElement = document.createElement('button');
     closeButtonElement.classList.add('modal-close');
-    closeButtonElement.innerText = 'close';
+    closeButtonElement.innerText = 'Close';
     closeButtonElement.addEventListener('click', hideModal);
 
     let titleElement = document.createElement('h1');
@@ -26,52 +23,32 @@ let modal = (function () {
     modal.appendChild(closeButtonElement);
     modal.appendChild(titleElement);
     modal.appendChild(contentElement);
-    modal.appendChild(modal);
+    modalContainer.appendChild(modal);
+
 
     modalContainer.classList.add('is-visible');
   }
-  
+
   function hideModal() {
-    mocalConatiner.classList.remove('is-visible');
-  }
-  
-  function showDialog(title, text) {
-      showModal(title, text);
-      //defining modalContainer:
-      let modalContainer = document.querySelector('#modal-container');
-      // add a confirm and cancel button to the modal:
-      let modal = modalContainer.querySelector('.modal');
-
-      let confirmButton = document.createElement('button');
-      confirmButton.classList.add('modal-confirm');
-      confirmButton.innerText = 'Confirm';
-
-      let cancelButton = document.createElement('button');
-      cancelButton.innerText = 'Cancel';
-
-      modal.appendChild(confirmButton);
-      modal.appendChild(cancelButton);
-
-      // User is supposed to be able to simply press Enter
-      confirmButton.focus();
-
+    modalContainer.classList.remove('is-visible');
   }
 
   window.addEventListener('keydown', (e) => {
     if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-      hideModal();
+      hideModal();  
     }
   });
   modalContainer.addEventListener('click', (e) => {
+    // Since this is also triggered when clicking INSIDE the modal
+    // We only want to close if the user clicks directly on the overlay
     let target = e.target;
     if (target === modalContainer) {
       hideModal();
     }
   });
-  // modal ends here
-  // return statement:
+
   document.querySelector('#show-modal').addEventListener('click', () => {
     showModal('Modal title', 'This is the modal content!');
   });
-})();
 
+})();
