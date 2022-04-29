9314a28b3a5b909f2153d2ace0d74cc0dd5e73b9
Sylvia <sylvia@donchev.is>
2022-04-29 18:41:28 +0200

    modal display

--- js-bootstrap/modal.js
+++ js-bootstrap/modal.js
@@ -1,64 +1,19 @@
-let modalContent = document.querySelector("#modal-content");
+let modalTitle = document.querySelector("#pokemonModalLabel");
+let modalBody = document.querySelector(".modal-body");
 
-//  eventListener to window for keyboard input esc
-window.addEventListener("keydown", (e) => {
-  console.log(`${e.key} was pressed`);
-  if (e.key === "Escape" && modalContent.classList.contains("is-visible")) {
-    hideModal();
-  }
-});
-
-function hideModal() {
-  modalContent.classList.remove("is-visible");
+//Global function
+function editModalContent(pokemon) {
+  modalTitle.innerText = pokemon.name;
+  modalBody.innerHTML = `<p>${pokemon.height}</p> <p>${pokemon.weight}</p>`;
 }
-
-//Enable specifying a title and content for the showModal function:
-function showModal(pokemonObj) {
-  modalContent.innerHTML = "";
-
-  let modal = document.createElement("div");
-  modal.classList.add("modal");
-
-  let closeButtonElement = document.createElement("button");
-  closeButtonElement.classList.add("modal-close");
-  closeButtonElement.innerText = "close";
-  closeButtonElement.addEventListener("click", hideModal);
-
-  let titleElement = document.querySelector("#pokemonModalLabel");
-  titleElement.innerText = pokemonObj.name;
-
-  let heightElement = document.createElement("p");
-  heightElement.innerText = "height: " + pokemonObj.height;
-
-  let weightElement = document.createElement("p");
-  weightElement.innerText = "weight: " + pokemonObj.weight;
-  let pokemonImg = document.createElement("img");
-  pokemonImg.src = pokemonObj.imageUrl;
-
-  modal.appendChild(closeButtonElement);
-  modal.appendChild(titleElement);
-  modal.appendChild(heightElement);
-  modal.appendChild(weightElement);
-  modal.appendChild(pokemonImg);
-  modalContent.appendChild(modal);
-  modalContent.classList.add("is-visible");
-}
-
-modalContent.addEventListener("click", (e) => {
-  // Since this is also triggered when clicking INSIDE the modal
-  // We only want to close if the user clicks directly on the overlay
-  let target = e.target;
-  if (target === modalContent) {
-    hideModal();
-  }
-});
-
 //creates button for pokemon
 function addListItem(pokemon) {
   let pokemonList = document.querySelector(".list-group");
   let listPokemon = document.createElement("li");
   listPokemon.classList.add("list-group-item");
   let button = document.createElement("button");
+  ///calling the global function
+  button.addEventListener("click", editModalContent(pokemon));
   button.innerText = pokemon.name;
   button.classList.add("btn", "btn-primary", "pokemon-button");
   // Changing DOM hierarchy
