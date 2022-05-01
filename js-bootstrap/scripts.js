01755f928578e8fcb9af09251d866ff1a1e90744
Sylvia <sylvia@donchev.is>
2022-05-01 18:43:13 +0200

    function removed

--- js-bootstrap/scripts.js
+++ js-bootstrap/scripts.js
@@ -67,27 +67,3 @@ async function loadDetails(pokemon) {
     console.error(e);
   }
 }
-
-function searchPokemon() {
-  // Declare variables
-  let input, filter, i, ul;
-  // User Input
-  input = document.getElementById("myInput");
-  // Filter, makes search not case sensitive
-  filter = input.value.toUpperCase();
-  // Grabs the parent element by id
-  ul = document.getElementById("list-group");
-
-  // Treats lists items like an array, where each item can be accessed through      it's index
-  for (i = 0; i < li.length; i++) {
-    item = li[i];
-    // Iterate over each list item to see if the value of the input, ignoring         case, matches the inner text or inner html of the item.
-    txtValue = item.textContent || item.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      // Displays list items that are a match, and nothing if no match
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-}
