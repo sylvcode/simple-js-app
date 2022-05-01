84aecccb6fbf67018b535a1b3f2a801efc98adcc
Sylvia <sylvia@donchev.is>
2022-05-01 18:26:05 +0200

    col bootstrap design

--- js-bootstrap/modal.js
+++ js-bootstrap/modal.js
@@ -1,24 +1,29 @@
 let modalTitle = document.querySelector("#pokemonModalLabel");
 let modalBody = document.querySelector(".modal-body");
 
-// let modalImage = document.querySelector(".modal-image");
-// modalImage.attr("src", pokemon.imageUrl);
-
 //Global function
 function editModalContent(pokemon) {
   modalTitle.innerText = pokemon.name;
   modalBody.innerHTML = `<p>🧨 Height: ${pokemon.height} m</p> 
   <p>💣 Weight: ${pokemon.weight} kg</p>
-   <p>🔋 Type: ${pokemon.type}</p>`;
-  // modalImage.innerText = `<img ${pokemon.imageUrl}/>`;
+   <p>🔋 Type: ${pokemon.type}</p>
+   <img src=${pokemon.imageUrl} alt="${pokemon.name}" />
+   <img src=${pokemon.imgUrl} alt=${pokemon.name} />`;
 }
 
 //creates button for pokemon
 function addListItem(pokemon) {
   let pokemonList = document.querySelector(".list-group");
+
   let listPokemon = document.createElement("li");
-  listPokemon.classList.add("list-group-item");
+  listPokemon.classList.add(
+    "group-list-item",
+    "col-lg-4",
+    "col-sm-12",
+    "col-md-6"
+  );
   let button = document.createElement("button");
+
   ///calling the global function
   button.addEventListener("click", () => editModalContent(pokemon));
   button.innerText = pokemon.name;
